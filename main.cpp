d1688dce3e51c9bb98bfa39762e9a4d05aee1373
Scott Bloom <scott_bloom@mentor.com>
2020-01-31 16:12:32 -0800

    Support for "power is number of digits" Handle a direct list of numbers

--- main.cpp
+++ main.cpp
@@ -7,10 +7,6 @@
 #include <list>
 #include <cctype>
 
-std::list< std::future< void > > sHandles;
-std::mutex sMutex;
-std::list< int64_t > sArmstrongNumbers;
-
 int fromChar( char ch, int base, bool & aOK )
 {
     if ( ch == '-' )
@@ -38,7 +34,6 @@ int fromChar( char ch, int base, bool & aOK )
     }
     return 0;
 }
-
 
 char toChar( int value )
 {
@@ -87,11 +82,11 @@ int64_t fromString( const std::string & str, int base )
     return retVal;
 }
 
-bool isArmstrong( int64_t val, int base, bool & aOK )
+bool isNarcissistic( int64_t val, int base, bool & aOK )
 {
     auto str = toString( val, base );
 
-    int64_t sumOfCubes = 0;
+    int64_t sumOfPowers = 0;
     int64_t value = 0;
     bool isNeg = false;
     for ( size_t ii = 0; ii < str.length(); ++ii )
@@ -109,141 +104,240 @@ bool isArmstrong( int64_t val, int base, bool & aOK )
             std::cerr << "Invalid character: " << currChar << std::endl;
             return false;
         }
-        sumOfCubes += static_cast<uint64_t>(std::pow( currVal, 3 ));
+        sumOfPowers += static_cast<uint64_t>(std::pow( currVal, str.length() ));
 
         value = (value * base) + currVal;
     }
 
-    return value == sumOfCubes;
+    return value == sumOfPowers;
 }
 
-void addArmstrongValue( int64_t value )
+class CNarcissisticNumCalculator
 {
-    std::lock_guard< std::mutex > lock( sMutex );
-    sArmstrongNumbers.push_back( value );
-}
-
-void computeCurrentBlock( int num, int base, int64_t min, int64_t max )
-{
-    //std::cout << "\n" << num << ": Computing for (" << min << "," << max-1 << ")" << std::endl;
-    int numArm = 0;
-    for ( auto ii = min; ii < max; ++ii )
+public:
+    bool parse( int argc, char ** argv )
     {
-        bool aOK = true;
-        bool isArmstrong = ::isArmstrong( ii, base, aOK );
-        if ( !aOK )
-            return;
-        if ( isArmstrong )
+        for ( int ii = 1; ii < argc; ++ii )
         {
-            //std::cout << curr << " is Armstrong? " << (isArmstrong ? "yes" : "no") << std::endl;
-            addArmstrongValue( ii );
-            numArm++;
+            bool aOK = false;
+            if ( strncmp( argv[ ii ], "-base", 5 ) == 0 )
+            {
+                fBase = getInt( ii, argc, argv, "-base", aOK );
+                if ( aOK && ((fBase < 2) || (fBase > 36)) )
+                {
+                    std::cerr << "Base must be between 2 and 36" << std::endl;
+                    aOK = false;
+                }
+            }
+            else if ( strncmp( argv[ ii ], "-max", 4 ) == 0 )
+            {
+                fMax = getInt( ii, argc, argv, "-max", aOK );
+            }
+            else if ( strncmp( argv[ ii ], "-thread_max", 11 ) == 0 )
+            {
+                fThreadMax = getInt( ii, argc, argv, "-max", aOK );
+            }
+            else if ( strncmp( argv[ ii ], "-numbers", 8 ) == 0 )
+            {
+                aOK = (ii + 1) < argc;
+                if ( !aOK )
+                {
+                    std::cerr << "-numbers requires a list of integers\n";
+                }
+                while ( aOK )
+                {
+                    auto curr = getInt( ii, argc, argv, "-numbers", aOK );
+                    if ( aOK )
+                        fNumbers.push_back( curr );
+                    if ( (ii + 1) >= argc )
+                        break;
+                }
+            }
+
+            if ( !aOK )
+                return false;
+        }
+        return true;
+    }
+
+    void run()
+    {
+        report();
+        createHandles();
+        while ( !isFinished() )
+            ;
+
+        reportFindings();
+    }
+private:
+    static int getInt( int & ii, int argc, char ** argv, const char * switchName, bool & aOK )
+    {
+        aOK = false;
+
+        if ( ++ii == argc )
+        {
+            std::cerr << "-base requires a value";
+            return 0;
+        }
+        const char * str = argv[ ii ];
+        int retVal = 0;
+        try
+        {
+            retVal = std::stoi( str );
+            aOK = true;
+        }
+        catch ( std::invalid_argument const & e )
+        {
+            std::cerr << switchName << " value '" << str << "' is invalid. \n" << e.what() << "\n";
+        }
+        catch ( std::out_of_range const & e )
+        {
+            std::cerr << switchName << " value '" << str << "' is out of range. \n" << e.what() << "\n";
+        }
+
+        return retVal;
+    }
+
+    void dumpNumbers( const std::list< int64_t > & numbers ) const
+    {
+        bool first = true;
+        size_t ii = 0;
+        for ( auto && currVal : numbers )
+        {
+            if ( ii && (ii % 5 == 0) )
+            {
+                std::cout << "\n";
+                first = true;
+            }
+            if ( !first )
+                std::cout << ", ";
+            else
+                std::cout << "    ";
+            first = false;
+            std::cout << toString( currVal, fBase ) << "(=" << currVal << ")";
+            ii++;
+        }
+        std::cout << "\n";
+    }
+
+    void reportFindings() const
+    {
+        std::cout << "There are " << fNarcissisticNumbers.size() << " Narcissistic numbers";
+        if ( fNumbers.empty() )
+            std::cout << " than or equal to " << fMax << "." << std::endl;
+        else
+            std::cout << " in the requested list." << std::endl;
+        fNarcissisticNumbers.sort();
+        dumpNumbers( fNarcissisticNumbers );
+    }
+
+    void report() const
+    {
+        if ( fNumbers.empty() )
+        {
+            std::cout << "Finding Narcissistic numbers up to: " << fMax << "\n";
+            std::cout << "Maximum Numbers per thread: " << fThreadMax << "\n";
+        }
+        std::cout << "Base : " << fBase << "\n";
+        if ( !fNumbers.empty() )
+        {
+            std::cout << "Checking if the following numbers are Narcissistic:\n";
+            dumpNumbers( fNumbers );
         }
     }
-    //std::cout << "\n" << num << ": ----> Computing for (" << min << "," << max-1 << ")" << " = " << numArm << std::endl;
-}
 
-void createHandles( int64_t min, int64_t max, int base, int computeMax )
-{
-    //std::cout << "Creating Handles for for (" << min << "," << max - 1 << ")" << std::endl;
-    int num = 0;
-    for ( auto ii = min; ii < max; ii += computeMax )
+    void findNarcissisticRange( int num, int base, int64_t min, int64_t max )
     {
-        sHandles.push_back( std::async( std::launch::async, computeCurrentBlock, num++, base, ii, ii + computeMax ) );
-    }
-}
-
-int getInt( int & ii, int argc, char ** argv, const char * switchName, bool & aOK )
-{
-    aOK = false;
-
-    if ( ii++ == argc )
-    {
-        std::cerr << "-base requires a value";
-        return 0;
-    }
-    const char * str = argv[ ii ];
-    int retVal = 0;
-    try
-    {
-        retVal = std::stoi( str );
-        aOK = true;
-    }
-    catch ( std::invalid_argument const & e )
-    {
-        std::cerr << switchName << " value '" << str << "' is invalid. \n" << e.what() << "\n";
-    }
-    catch ( std::out_of_range const & e )
-    {
-        std::cerr << switchName << " value '" << str << "' is out of range. \n" << e.what() << "\n";
-    }
-
-    return retVal;
-}
-
-bool parseCommandLine( int argc, char ** argv, int & base, int & max, int & computeMax )
-{
-    for ( int ii = 1; ii < argc; ++ii )
-    {
-        bool aOK = false;
-        if ( strncmp( argv[ ii ], "-base", 5 ) == 0 )
+        //std::cout << "\n" << num << ": Computing for (" << min << "," << max-1 << ")" << std::endl;
+        int numArm = 0;
+        for ( auto ii = min; ii < max; ++ii )
         {
-            base = getInt( ii, argc, argv, "-base", aOK );
-            if ( aOK && ((base < 2) || (base > 36)) )
+            bool aOK = true;
+            bool isNarcissistic = ::isNarcissistic( ii, base, aOK );
+            if ( !aOK )
+                return;
+            if ( isNarcissistic )
             {
-                std::cerr << "Base must be between 2 and 36" << std::endl;
-                aOK = false;
+                //std::cout << curr << " is Narcissistic? " << (isNarcissistic ? "yes" : "no") << std::endl;
+                addNarcissisticValue( ii );
+                numArm++;
             }
         }
-        else if ( strncmp( argv[ ii ], "-max", 4 ) == 0 )
-        {
-            max = getInt( ii, argc, argv, "-max", aOK );
-        }
-        else if ( strncmp( argv[ ii ], "-compute_max", 12 ) == 0 )
-        {
-            computeMax = getInt( ii, argc, argv, "-max", aOK );
-        }
-        if ( !aOK )
-            return false;
+        //std::cout << "\n" << num << ": ----> Computing for (" << min << "," << max-1 << ")" << " = " << numArm << std::endl;
     }
-    return true;
-}
 
-bool isFinished()
-{
-    while ( !sHandles.empty() )
+    void findNarcissisticList( int base, const std::list< int64_t > & values )
     {
-        for ( auto ii = sHandles.begin(); ii != sHandles.end(); )
+        int numArm = 0;
+        for ( auto ii : values )
         {
-            if ( (*ii).wait_for( std::chrono::seconds( 0 ) ) == std::future_status::ready ) // finished
-                ii = sHandles.erase( ii );
-            else
-                break; // still running
+            bool aOK = true;
+            bool isNarcissistic = ::isNarcissistic( ii, base, aOK );
+            if ( !aOK )
+                return;
+            if ( isNarcissistic )
+            {
+                //std::cout << curr << " is Narcissistic? " << (isNarcissistic ? "yes" : "no") << std::endl;
+                addNarcissisticValue( ii );
+                numArm++;
+            }
         }
+        //std::cout << "\n" << num << ": ----> Computing for (" << min << "," << max-1 << ")" << " = " << numArm << std::endl;
     }
-    return sHandles.empty();
-}
+
+    void createHandles()
+    {
+        //std::cout << "Creating Handles for for (" << min << "," << max - 1 << ")" << std::endl;
+        if ( fNumbers.empty() )
+        {
+            int num = 0;
+            for ( auto ii = 0; ii < fMax; ii += fThreadMax )
+            {
+                sHandles.push_back( std::async( std::launch::async, &CNarcissisticNumCalculator::findNarcissisticRange, this, num++, fBase, ii, ii + fThreadMax ) );
+            }
+        }
+        else
+            sHandles.push_back( std::async( std::launch::async, &CNarcissisticNumCalculator::findNarcissisticList, this, fBase, fNumbers ) );
+    }
+
+    bool isFinished()
+    {
+        while ( !sHandles.empty() )
+        {
+            for ( auto ii = sHandles.begin(); ii != sHandles.end(); )
+            {
+                if ( (*ii).wait_for( std::chrono::seconds( 0 ) ) == std::future_status::ready ) // finished
+                    ii = sHandles.erase( ii );
+                else
+                    break; // still running
+            }
+        }
+        return sHandles.empty();
+    }
+
+    void addNarcissisticValue( int64_t value )
+    {
+        std::lock_guard< std::mutex > lock( fMutex );
+        fNarcissisticNumbers.push_back( value );
+    }
+
+    int fBase{ 10 };
+    int fMax{ 100000 };
+    int fThreadMax{ 100 };
+    std::list< int64_t > fNumbers;
+    std::list< std::future< void > > sHandles;
+
+    std::mutex fMutex;
+    mutable std::list< int64_t > fNarcissisticNumbers;
+};
 
 int main( int argc, char ** argv )
 {
-    int base = 10;
-    int max = 100000;
-    int computeMax = 100;
-    if ( !parseCommandLine( argc, argv, base, max, computeMax ) )
+    CNarcissisticNumCalculator values;
+    if ( !values.parse( argc, argv ) )
         return 1;
 
-    std::cout << "Finding Narcissistic numbers up to: " << max << "\n";
-    std::cout << "Maximum Numbers per thread: " << computeMax << "\n";
-    std::cout << "Base : " << base << "\n";
-    createHandles( 0, max, base, computeMax );
-    while ( !isFinished() )
-        ;
-
-    std::cout << "There are " << sArmstrongNumbers.size() << " armstrong numbers less than or equal to " << max << std::endl;
-    sArmstrongNumbers.sort();
-    for ( auto && ii : sArmstrongNumbers )
-    {
-        std::cout << ii << "\n";
-    }
+    values.run();
+    return 0;
 }
 
